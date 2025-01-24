501e800e42b2acfb9a92ff27bdbd8b0873c3d368
Bogdan Bošković <bogdano@users.noreply.github.com>
2025-01-24 10:14:10 -0500

    lickanje, latinica, engleski, cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,20 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import sitemap from '@astrojs/sitemap';
+
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), sitemap()],
+  site: "https://studentskizahtevi.com",
+  output: "static",
+  prefetch: true,
+
+  experimental: {
+      clientPrerender: true,
+  },
+
+  adapter: cloudflare(),
 });
